6b8805307db68b5904026be988489e09f0b125b5
AmanTiwari1503 <tiwariaman2000@gmail.com>
2019-02-27 01:39:05 +0530

    Updated manager page

--- client/scripts/request.js
+++ client/scripts/request.js
@@ -58,6 +58,64 @@ function setter(id){
   else if(id=="stat"){
 
   }
+  else if(id=="coun"){
+    usr.innerHTML += "<div style='text-align:center;color:white'>Your current country is :</div><br><br>"
+    usr.innerHTML += '<div style="text-align:center;color:white">Update your country:<br><br><div class="selector">\
+            <div class="input-select" align="center" width="480" heig>\
+              <select data-trigger="" name="choices-single-default">\
+                <option placeholder="">Category</option>\
+                <option onclick="poster();">PLAYER NAME</a></option>\
+                <option>PLAYER ID</option>\
+                <option>NATIONALITY</option>\
+                <option>AGE</option>\
+                <option>OVERALL RATING</option>\
+                <option>TEAM</option>\
+                <option>PLAYING POSITION</option>\
+              </select>\
+            </div>\
+          </div>\
+          </div>'
+  }
+  else if(id=="cplayer"){
+    usr.innerHTML += "<h1 style='text-align:center;color:white;'>Create New Player</h1><br>"
+    usr.innerHTML += "<form style='text-align:center;padding:auto;color:white;'>\
+                      <label for='pname'>Player Name</label>\
+                     <input type='text' id='pname' name='pname' required><br><br>\
+                     <label for='dob'>Date of Birth</label>\
+                     <input type='text' id='dob' name='dob' required><br><br>\
+                     <label for='pname'>Height</label>\
+                     <input type='text' id='height' name='height' required><br><br>\
+                     <label for='pname'>Weight</label>\
+                     <input type='text' id='weight' name='weight' required><br><br>\
+                     <button type='submit' style='padding:5px'>Submit</button>\
+                     </form>\
+                     "
+  }
+  else if(id=="cteam"){
+    usr.innerHTML += "<h1 style='text-align:center;color:white'>Create New Team</h1><br>"
+    usr.innerHTML += "<form style='text-align:center;color:white;padding:auto;'>\
+                      <label for='pname'>Team Name</label>\
+                     <input type='text' id='pteam' name='pteam' required><br><br>\
+                     <label for='dob'>Short Name</label>\
+                     <input type='text' id='sn' name='sn' required><br><br>\
+                     <label for='league'>League</label><br><br>\
+                     <div class='selector'  id='league'>\
+            <div class='input-select' width='480' heig>\
+              <select data-trigger='' name='choices-single-default'>\
+                <option placeholder=''>Category</option>\
+                <option onclick='poster();''>PLAYER NAME</a></option>\
+                <option>PLAYER ID</option>\
+                <option>NATIONALITY</option>\
+                <option>AGE</option>\
+                <option>OVERALL RATING</option>\
+                <option>TEAM</option>\
+                <option>PLAYING POSITION</option>\
+              </select>\
+            </div>\
+          </div>\
+          </form>\
+          "
+  }
 
 }
 function playerPost(){
