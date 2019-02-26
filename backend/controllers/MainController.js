42410f2294a16d4f98f2c502d533580c9fc2d583
Pratyush <yashpandey4@gmail.com>
2019-02-26 22:46:08 +0530

    error fixes

--- backend/controllers/MainController.js
+++ backend/controllers/MainController.js
@@ -214,7 +214,7 @@ ORDER BY Country.name, League.name, season DESC\
 function playersByBirthYear(req,res){
     var query = "SELECT COUNT(p.player_name) AS number_of_players, \
 --strftime('%Y',p.birthday) AS \"year_born\"\
-EXTRACT(YEAR FROM to_timestamp(p.birthday, 'YYYY-MM-DD hh24:mi:ss')) AS "year_born"\
+EXTRACT(YEAR FROM to_timestamp(p.birthday, 'YYYY-MM-DD hh24:mi:ss')) AS \"year_born\"\
 FROM Player AS p\
 INNER JOIN Player_Attributes AS pa \
 ON p.player_api_id = pa.player_api_id\
